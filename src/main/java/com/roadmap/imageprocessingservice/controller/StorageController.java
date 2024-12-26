f0e3b793046f9c473655cf3595fc8603ce1c682b
adityabeniwal <93305298+adityabeniwal@users.noreply.github.com>
2024-12-26 21:14:12 +0530

    adding aws config and service

---
+++ src/main/java/com/roadmap/imageprocessingservice/controller/StorageController.java
@@ -0,0 +1,16 @@
+package com.roadmap.imageprocessingservice.controller;
+
+import com.roadmap.imageprocessingservice.service.AwsStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+public class StorageController {
+
+
+}
--- src/main/java/com/roadmap/imageprocessingservice/service/ImageProcessingService.java
+++ src/main/java/com/roadmap/imageprocessingservice/service/ImageProcessingService.java
@@ -1,5 +1,19 @@
 package com.roadmap.imageprocessingservice.service;
 
 
-public class ImageProcessingService {
+import org.imgscalr.Scalr;
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class ImageProcessingService
+{
+
+
+
+
+    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws Exception {
+        return Scalr.resize(originalImage, Scalr.Method.AUTOMATIC, Scalr.Mode.AUTOMATIC, targetWidth, targetHeight, Scalr.OP_ANTIALIAS);
+    }
 }
